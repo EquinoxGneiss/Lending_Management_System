db9a96b1b284c956d4c8bb5381153aa7772eaf11
Justin Cruz <cruz34621@gmail.com>
2021-12-10 14:50:35 +0800

    Design Modified - Loan Page Design - Main Page Design Update

--- Finals/Main.Designer.vb
+++ Finals/Main.Designer.vb
@@ -97,16 +97,16 @@ Partial Class Main
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Loan"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(142, 81)
+        Me.Button2.Location = New System.Drawing.Point(158, 81)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 23)
         Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Button2"
+        Me.Button2.Text = "Withdaw"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
@@ -115,7 +115,7 @@ Partial Class Main
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(75, 23)
         Me.Button3.TabIndex = 3
-        Me.Button3.Text = "Button3"
+        Me.Button3.Text = "Deposit"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
@@ -124,7 +124,7 @@ Partial Class Main
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(75, 23)
         Me.Button4.TabIndex = 4
-        Me.Button4.Text = "Button4"
+        Me.Button4.Text = "Manage"
         Me.Button4.UseVisualStyleBackColor = True
         '
         'Main
@@ -132,8 +132,7 @@ Partial Class Main
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
-        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(1350, 729)
+        Me.ClientSize = New System.Drawing.Size(1350, 707)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
